5cddf75d71655fa921fdc86447d05de89da84ead
Rosalyn Goh <rosalyngoh@Rosalyns-iMac.local>
2015-08-11 16:38:02 -0700

    finished up js 101 prob 3

--- javascript_101/problem_3.js
+++ javascript_101/problem_3.js
@@ -1,9 +1,11 @@
 var primeFactors = [];
-var divideMe = 13195;
+var divideMe = 600851475143;
+var divideMe1 = 600851475143;
 var remainder = 0;
+var largest = 0;
 
 var isPrime = function (k) {
-	for (j = 0; j < 10; j++) {
+    for (j = 0; j < 10; j++) {
         if (k % j != 0) {
             return true;
         }
@@ -26,5 +28,10 @@ for (i = 0; i <= divideMe; i++) {
     }
 }
 
-console.log(primeFactors);
+for (i = 0; i < primeFactors.length; i++) {
+    if (primeFactors[i] > largest) {
+        largest = primeFactors[i];
+    }
 }
+
+console.log(largest + " is the largest prime factor for " + divideMe1);
