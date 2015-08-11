7d29b2bb1c65c48aa75a2d59aa3b839e8ae74183
Rosalyn Goh <rosalyn.goh@gmail.com>
2015-08-11 09:13:06 -0700

    attempt recursion to solve for divisibility

--- javascript_101/problem_3.js
+++ javascript_101/problem_3.js
@@ -1,10 +1,14 @@
 var factors = [];
 var remainder = 13195;
 
+var divisible = function (z) {
+	
+}
+
 for (i = 2; i <= 13195; i++) {
 	if (remainder % i === 0) {
 		for (j = 0; j < 10; j++) {
-			
+
 		}
 	}
 }
