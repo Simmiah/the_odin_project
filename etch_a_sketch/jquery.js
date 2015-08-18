4d2d3b4a5491152f69cd75b4e056c6ac63244001
Rosalyn Goh <rosalyn.goh@gmail.com>
2015-08-18 08:20:04 -0700

    etch a sketch trying out floats

--- etch_a_sketch/jquery.js
+++ etch_a_sketch/jquery.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	
+	for (y=1; y <= 16; y++) {
+		if (y != 16) {
+			$('#container').append('<div class="square right"></div>');			
+		} else {
+			$('#container').append('<div class="square right"></div>');
+		}
+		//for (x=1; x<=16; x++) {
+		//	$('#container').append('<div class="square"></div>');
+		//}
+	}
+
+	//$('#container').append('<div class="square"></div>');
 });
