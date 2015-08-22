c3124ea78437289008641fdfed528048b13e4d29
Rosalyn Goh <rosalyn.goh@gmail.com>
2015-08-21 21:39:03 -0700

    etch a sketch initialized the grid with jquery

--- etch_a_sketch/jquery.js
+++ etch_a_sketch/jquery.js
@@ -1,14 +1,13 @@
-$(document).ready(function(){
-	for (y=1; y <= 16; y++) {
-		if (y != 16) {
-			$('#container').append('<div class="square right"></div>');			
-		} else {
-			$('#container').append('<div class="square right"></div>');
-		}
-		//for (x=1; x<=16; x++) {
-		//	$('#container').append('<div class="square"></div>');
-		//}
-	}
+var grid = 4;
 
-	//$('#container').append('<div class="square"></div>');
+$(document).ready(function(){
+	for (x=0; x<grid; x++) {
+		for (y=0; y<=grid; y++) {
+            if (y != grid) {
+                $('#container').append('<div class="square right"></div>');			
+            } else {
+                $('#container').append('<div class="clear"></div>');
+            }
+        }
+	}
 });
