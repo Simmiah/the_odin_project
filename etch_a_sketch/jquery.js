0d09c7773f68e21f616393f2a58680700758fc81
Rosalyn Goh <rosalyn.goh@gmail.com>
2015-08-22 22:21:33 -0700

    etch a sketch: finished basic etch a sketch functionality

--- etch_a_sketch/jquery.js
+++ etch_a_sketch/jquery.js
@@ -1,16 +1,29 @@
-var grid = 4;
+var grid = 5;
 
 $(document).ready(function(){
-	for (x=0; x<grid; x++) {
-		for (y=0; y<=grid; y++) {
-            if (y != grid) {
+	makeGrid(grid);
+    $('.square').hover(function(){
+        $(this).addClass('black');
+    });
+    
+    $('button').click(function(){
+    	$('.square').remove();
+        var grid = prompt("Enter number of boxes per side in grid");
+        makeGrid(grid);
+        $('.square').hover(function(){
+            $(this).addClass('black');
+        });
+    });
+});
+
+function makeGrid(grid){
+    for (y=0; y<grid; y++) {
+		for (x=0; x<=grid; x++) {
+            if (x != grid) {
                 $('#container').append('<div class="square right"></div>');			
             } else {
                 $('#container').append('<div class="clear"></div>');
             }
         }
 	}
-	$('.square').hover(function(){
-		$(this).addClass('black');
-	});
-});
+}
