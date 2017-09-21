39f9bce9b89b08c650da5d237be32cd7df33675c
Rosalyn Goh <rosalyn.goh@gmail.com>
2017-09-21 12:55:56 -0700

    cleaned up spacing

--- etch_a_sketch/jquery.js
+++ etch_a_sketch/jquery.js
@@ -2,28 +2,28 @@ var grid = 5;
 
 $(document).ready(function(){
 	makeGrid(grid);
+  $('.square').hover(function(){
+    $(this).addClass('black');
+  });
+
+  $('button').click(function(){
+    $('.square').remove();
+    var grid = prompt("Enter number of boxes per side in grid");
+    makeGrid(grid);
     $('.square').hover(function(){
-        $(this).addClass('black');
+      $(this).addClass('black');
     });
-    
-    $('button').click(function(){
-    	$('.square').remove();
-        var grid = prompt("Enter number of boxes per side in grid");
-        makeGrid(grid);
-        $('.square').hover(function(){
-            $(this).addClass('black');
-        });
-    });
+	});
 });
 
 function makeGrid(grid){
-    for (y=0; y<grid; y++) {
+  for (y=0; y<grid; y++) {
 		for (x=0; x<=grid; x++) {
-            if (x != grid) {
-                $('#container').append('<div class="square right"></div>');			
-            } else {
-                $('#container').append('<div class="clear"></div>');
-            }
-        }
+      if (x != grid) {
+        $('#container').append('<div class="square right"></div>');
+      } else {
+        $('#container').append('<div class="clear"></div>');
+      }
+    }
 	}
 }
